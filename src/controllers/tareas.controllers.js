4a174f47f5f7b50f2e7ce3f668a62e45c2f98f3a
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-03-15 18:45:50 -0300

    se agrego la funcion para obtener la tarea por su id

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -1,16 +1,27 @@
 import Tarea from "../database/model/tarea.js";
 
 export const listarTareas = async (req, res) => {
-    try{
-        const tareas = await Tarea.find();
-        res.status(200).json(tareas)
-    }catch (error){
-        console.log(error);
-        res.status(404).json({
-            mensaje: "No se pudo encontrar la lista de tareas",
-          });
-    }
-  
+  try {
+    const tareas = await Tarea.find();
+    res.status(200).json(tareas);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "No se pudo encontrar la lista de tareas",
+    });
+  }
+};
+
+export const obtenerTarea = async (req, res) => {
+  try {
+    const tareaBuscada = await Tarea.findById(req.params.id);
+    res.status(200).json(tareaBuscada);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "No se pudo encontrar la tarea",
+    });
+  }
 };
 
 export const crearTarea = async (req, res) => {
