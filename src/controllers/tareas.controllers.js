30fb061257d89d1e67fafc3bd46b4b0d787d5f1e
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-03-15 17:16:42 -0300

    se agrego el Schems de tareas

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -1,3 +1,5 @@
+import Tarea from '../database/model/tarea.js'
+
 export const listarTareas = (req, res)=>{
     console.log('desde listar tareas');
     res.send('enviar lista de tareas...')
---
+++ src/database/model/tarea.js
@@ -0,0 +1,15 @@
+import mongoose, {Schema} from "mongoose";
+
+const tareaSchema = new Schema({
+    tarea:{
+        type: String,
+        required: true,
+        unique: true,
+        minLength:3,
+        maxLength:40
+    }
+})
+
+const Tarea = mongoose.model('tarea', tareaSchema);
+
+export default Tarea;
