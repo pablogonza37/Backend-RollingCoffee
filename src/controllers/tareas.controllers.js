aabcadf4a6c004ce0e5cd4cf614fccb6e6a9c35c
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-03-15 18:19:22 -0300

    se agregaron datos de prueba a la base de datos

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -1,6 +1,21 @@
-import Tarea from '../database/model/tarea.js'
+import Tarea from "../database/model/tarea.js";
 
-export const listarTareas = (req, res)=>{
-    console.log('desde listar tareas');
-    res.send('enviar lista de tareas...')
-}
+export const listarTareas = (req, res) => {
+  console.log("desde listar tareas");
+  res.send("enviar lista de tareas...");
+};
+
+export const crearTarea = async (req, res) => {
+  try {
+    const tareaNueva = new Tarea(req.body);
+    await tareaNueva.save();
+    res.status(201).json({
+      mensaje: "La tarea fue creada correctamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      mensaje: "No se pudo procesar la solicitud de crear la tarea",
+    });
+  }
+};
